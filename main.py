441fc973efa580edad307ad9fa9e561d95cfe2ce
syrbor <skichuk@gmail.com>
2022-08-14 22:48:52 +0300

    import data from json

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from utils import load_candidates_from_json, get_candidates_by_skill, get_candidate, get_candidates_by_name
 
 app = Flask(__name__)
+
+FILEPATH = 'candidates.json'
+
+data = load_candidates_from_json(FILEPATH)
 
 
 if __name__ == '__main__':
